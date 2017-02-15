383ecf576e0a8d69e358d406bf3624f9859d9bb6
Nick White <nick41496@gmail.com>
2017-02-14 21:26:43 -0500

    Add .log to .gitignore and change app.js port

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ const express = require('express');
 
 const app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-app.listen(3000, () => {
-    console.log('Example app listening on port 3000!')
+app.listen(app.get('port'), () => {
+    console.log(`Node app is running on port ${app.get('port')}`);
 });
