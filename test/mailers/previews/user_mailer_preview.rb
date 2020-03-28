3b6b16eacb5f4ff86f5376250ee0853fafc58cb2
Nick White <nick@nickwhite.co>
2020-03-28 11:33:46 -0700

    Remove test/ in preparation for adding rspec

--- test/mailers/previews/user_mailer_preview.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class UserMailerPreview < ActionMailer::Preview
-  def likes_update
-    User.first.twitter_accounts.update_all(latest_tweet_id: nil)
-
-    UserMailer.with(user: User.first).likes_update
-  end
-end
