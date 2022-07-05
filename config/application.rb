8985351d893aeb290b8ff08f934f428a860e1490
Nick White <nick@nickwhite.co>
2022-07-04 23:00:45 -0700

    Use Postmark for email sending
    This ought to improve successful sends and decrease spam rate.

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,10 @@ module Likelater
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.action_mailer.delivery_method = :postmark
+    config.action_mailer.postmark_settings = {
+      api_token: ENV["POSTMARK_API_TOKEN"],
+    }
     config.action_mailer.preview_path = "#{Rails.root}/spec/mailers/previews"
   end
 end
